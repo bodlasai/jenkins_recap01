6c7e32f2f06c329420ca9c4968d9c8ebb67ab3bb
bodla sai <30978046+bodlasai@users.noreply.github.com>
2021-12-17 09:24:16 +0530

    test1

--- hello.java
+++ hello.java
@@ -1,4 +1,4 @@
 public class hello{
   public static void main(String[] args){
-    System.out.println("hello jenkins master and slave ");
+    System.out.println("hello jenkins master and slave. ");
 }}
